c0a3bfd414d635b30fb8d8615cd4418040c8aa3f
eli <john.berengu@student.moringaschool.com>
2024-11-30 15:20:26 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+// code your solution heresuter
+saturdayFun=(toDo = "roller-skate")=>{
+        return `This Saturday, I want to ${toDo}!`;
+    }
+
+function mondayWork(toDo= "go to the office") {
+    return `This Monday, I will ${toDo}.`;
+}
+
+function wrapAdjective(emphatic) {
+    return function(adjective) {
+      return `You are ${emphatic}${adjective}${emphatic}!`;
+    };
+  }
